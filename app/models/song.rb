ef2d2a5985e95183cfa3b1c2a268509c777f2113
Tammy Lam <lamtammy1515@gmail.com>
2020-06-21 22:24:05 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,47 +4,25 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
   
- def genre_name=(name)
-    genre = Genre.find_or_create_by(name: name)
-    self.genre = genre
+ def artist=(name)
+    self.artist_id = Artist.find_or_create_by(name: name).id
   end
 
-  def genre_name
-    self.try(:genre).try(:name)
-  end    
-
-  def artist_name
-    self.try(:artist).try(:name)
-  end
-  
-  def artist_name=(name)
-    artist = Artist.find_or_create_by(name: name)
-    self.artist = artist
-  end
-
-  def note_contents=(contents)
-    contents.each do |content|
-      if !content.empty?
-          note = Note.create(content: content, song_id: self.id)
-          self.notes << note
-      end
+  def song_notes=(notes)
+    notes.each do |note|
+      self.notes.build(content: note)
     end
   end
 
-  # This implementation is a little more advanced,
-  # but allows notes to have separate ids to avoid
-  # issues in a more realistic scenario if a song and
-  # its notes were deleted
-
-  #def note_contents=(contents)
-  #  contents.delete_if(&:blank?).each { |content|
-  #    if !Note.find_by(content: content, song_id: self.id)
-  #    self.notes.build(content: content)
-  #  end
-  #  }
-  #end
+  def genre=(id)
+    self.genre_id = id.to_i
+  end
 
   def note_contents
-    self.notes.map { |note| note.content }
+    content = []
+    self.notes.each do |note|
+      content << note.content
+    end
+    content
   end
 end
