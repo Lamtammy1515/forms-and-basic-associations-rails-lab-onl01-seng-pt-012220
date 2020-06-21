4609f1a5c49ab549c3f8a329b19e3438fdc7f02b
Tammy Lam <lamtammy1515@gmail.com>
2020-06-21 22:06:41 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,29 +4,47 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
   
-    def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name)
-  end
-
-  def artist_name
-    self.artist ? self.artist.name : ''
-  end
-
-  def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: name)
+ def genre_name=(name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
   end
 
   def genre_name
-    self.genre ? self.genre.name : ''
+    self.try(:genre).try(:name)
+  end    
+
+  def artist_name
+    self.try(:artist).try(:name)
   end
+  
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      if !content.empty?
+          note = Note.create(content: content, song_id: self.id)
+          self.notes << note
+      end
+    end
+  end
+
+  # This implementation is a little more advanced,
+  # but allows notes to have separate ids to avoid
+  # issues in a more realistic scenario if a song and
+  # its notes were deleted
+
+  #def note_contents=(contents)
+  #  contents.delete_if(&:blank?).each { |content|
+  #    if !Note.find_by(content: content, song_id: self.id)
+  #    self.notes.build(content: content)
+  #  end
+  #  }
+  #end
 
   def note_contents
-    notes.collect {|note| note.content}
-  end
-
-  def note_contents=(notes)
-    notes.each do |content|
-      note = self.notes.build(content: content) unless content.empty?
-    end
+    self.notes.map { |note| note.content }
   end
 end
